dc95e8e29e90820fae7970d88833826e78b1da71
nnassrull <nasrul2001@gmail.com>
2025-03-26 01:40:00 +0530

    Added new types

---
+++ src/types/admin.types.ts
@@ -0,0 +1,16 @@
+// Admin Profile types
+export interface AdminProfile {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  username: string;
+  role: string;
+}
+
+export interface AdminProfileUpdate {
+  name: string;
+  email: string;
+  phone: string;
+  username: string;
+} 

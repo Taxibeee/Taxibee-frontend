243665f48327be34098119d973d18b54b1b8a4c6
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2025-04-21 01:24:23 +0530

    Line Chart Wrapper added.

---
+++ src/components/charts/LineChartWrapper.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { LineChart } from '@mui/x-charts';
+
+interface LineChartWrapperProps {
+  dataset: any;
+
+  xAxis: any;
+  yAxis: any;
+
+  series: any;
+
+  colors: any;
+}
+
+const LineChartWrapper: React.FC<LineChartWrapperProps> = ({ dataset, xAxis, yAxis, series, colors }) => {
+
+  return (
+    <LineChart
+      dataset = {dataset}
+      xAxis = {xAxis}
+      yAxis = {yAxis}
+      series = {series}
+      height = {400}
+      colors={colors}
+    />
+  );
+};
+
+export default LineChartWrapper;

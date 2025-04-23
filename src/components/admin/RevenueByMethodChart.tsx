42913c5a87b89a4fe05989152da158b4b7ee61b3
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2025-04-23 20:58:39 +0530

    Pie Chart updated.

--- src/components/admin/RevenueByMethodChart.tsx
+++ src/components/admin/RevenueByMethodChart.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Card, Skeleton, CardContent, Typography, Box } from '@mui/material';
+import { Typography } from '@mui/material';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { useAdminQueries } from '../../hooks';
-import { CustomAlert } from '../../utils/customAlert';
+import FlexWrapper from '../common/FlexWrapper';
+import CardWrapper from '../common/CardWrapper';
+
 
 // Utility function for formatting currency
 const formatCurrency = (amount: number | undefined | null) => {
@@ -58,82 +60,49 @@ const RevenueByMethodChart: React.FC<RevenueByMethodChartProps> = ({ startDate, 
   };
 
   return (
-    <Card elevation={1} sx={{ p: 0 }}>
-      <CardContent>
-        {isLoading ? (
-          <Box
-            sx={{
-              height: 350,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              p: 0,
-            }}
-          >
-            <Skeleton
-              variant="rectangular"
-              width="100%"
-              height="100%"
-              animation="wave"
-              sx={{
-                transform: 'none', // This prevents the skeleton from being squished
-              }}
-            />
-          </Box>
-        ) : isError ? (
-          <CustomAlert severity="error">Failed to load payment method data.</CustomAlert>
-        ) : !data || Object.keys(data).length === 0 ? (
-          <Typography>No payment data available.</Typography>
-        ) : (
-          <Box
-            sx={{
-              height: 350,
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <PieChart
-              series={[
-                {
-                  data: prepareChartData(),
-                  highlightScope: { faded: 'global', highlighted: 'item' },
-                  innerRadius: 100,
-                  paddingAngle: 0,
-                  cornerRadius: 4,
-                },
-              ]}
-              height={300}
-              width={300}
-              margin={{ top: 10, bottom: 10, left: 10, right: 10 }}
-              slotProps={{
-                legend: {
-                  direction: 'column',
-                  position: { vertical: 'middle', horizontal: 'right' },
-                  padding: 0,
-                  labelStyle: {
-                    fontSize: 12,
-                  },
-                  itemMarkWidth: 10,
-                  itemMarkHeight: 10,
-                  markGap: 5,
-                  itemGap: 15,
-                },
-              }}
-            />
-            <Box sx={{ alignSelf: 'flex-start', ml: 2, display: 'flex', flexDirection: 'column' }}>
-              <Typography variant="subtitle1" sx={{ mt: 2, fontWeight: 'bold' }} component="span">
-                Total Revenue: {formatCurrency(calculateTotal())}
-              </Typography>
-              <Typography variant="caption" color="textSecondary" sx={{ mt: 1 }}>
-                Revenue by payment method
-              </Typography>
-            </Box>
-          </Box>
-        )}
-      </CardContent>
-    </Card>
+
+
+    <CardWrapper isLoading={isLoading}>
+      <FlexWrapper direction='vertical'>
+        <PieChart
+          series={[
+            {
+              data: prepareChartData(),
+              highlightScope: { faded: 'global', highlighted: 'item' },
+              innerRadius: 100,
+              paddingAngle: 0,
+              cornerRadius: 4,
+            },
+          ]}
+          height={300}
+          width={300}
+          margin={{ top: 10, bottom: 50, left: 10, right: 10 }}
+          slotProps={{
+            legend: {
+              direction: 'row',
+              position: { vertical: 'bottom', horizontal: 'middle' },
+              padding: 8,
+              labelStyle: {
+                fontSize: 12,
+              },
+              itemMarkWidth: 10,
+              itemMarkHeight: 10,
+              markGap: 5,
+              itemGap: 15,
+              hidden: false,
+            },
+          }}
+        />
+        <FlexWrapper direction='vertical'>
+          <Typography variant="subtitle1" sx={{ mt: 2, fontWeight: 'bold' }} component="span">
+            Total Revenue: {formatCurrency(calculateTotal())}
+          </Typography>
+          <Typography variant="caption" color="textSecondary" sx={{ mt: 1 }}>
+            Revenue by payment method
+          </Typography>
+        </FlexWrapper>
+      </FlexWrapper>
+    </CardWrapper>
   );
 };
 
